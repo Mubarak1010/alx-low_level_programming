d3ac5f09701566dde7b388154439c3d6a844974d
Mubarak1010 <anoze941@gmail.com>
2023-02-17 12:23:03 -0800

    positive no. less than 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing number from 0 - 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+	return (0);
+}
