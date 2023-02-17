8e827f7a10c3a6ca8c55b224dfd99782ab8fbd6b
Mubarak1010 <anoze941@gmail.com>
2023-02-17 12:14:24 -0800

    numbers less that 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing single numbers of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf("\n");
+	return (0);
+}
