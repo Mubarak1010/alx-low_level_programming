564ee5fbf820a66eeba7ed71af0ca55dfb79c0a8
Mubarak1010 <anoze941@gmail.com>
2023-02-21 13:24:41 -0800

    header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - program that prints _putchar
+ * and returns new line
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
