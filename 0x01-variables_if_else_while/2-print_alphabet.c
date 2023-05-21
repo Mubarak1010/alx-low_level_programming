b5c69fc20df6a6fb93efac488447e2be4d712912
Mubarak1010 <anoze941@gmail.com>
2023-05-21 12:49:33 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,7 @@ int main(void)
 {
 	int i;
 
-	char alp[26] = {a, b, c, d, e, f, g, h, i, j, k, l,
-		m, n, o, p, q, r, s, t, u, v, w, x, y, z};
+	char alp[26] = {abcdefghijklmnopqrstuvwxyz};
 
 	for (i = 0; i <= 26; i++)
 		putchar('alp[i]');
