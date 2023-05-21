cbc07445b0a663d53c0a5612685e81c1bf17ee79
Mubarak1010 <anoze941@gmail.com>
2023-05-21 13:40:57 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Pragram entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp != 'e' && alp != 'q')
+			putchar(alp);
+	}
+	putchar('\n');
+}
