f3b8c83d25fe8c32e3030393f4032081a007c71d
Mubarak1010 <anoze941@gmail.com>
2023-05-21 13:14:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; i <= 'z'; i++)
+		putchar('alp');
+	putchar('\n');
+
+	return (0);
+}
