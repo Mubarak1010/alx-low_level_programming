45986c386c6d36bb4119d0562df08ea9d46785b9
Mubarak1010 <anoze941@gmail.com>
2023-05-24 14:53:10 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("\n");
+	return (0);
+}
