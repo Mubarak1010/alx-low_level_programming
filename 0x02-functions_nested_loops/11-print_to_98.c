83c4295f154ddaf7ca9c8eb4f2c9df0d77240adf
Mubarak1010 <anoze941@gmail.com>
2023-05-24 13:47:42 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
 		printf("%d", n);
 		if (n != 98)
@@ -18,7 +18,7 @@ void print_to_98(int n)
 		}
 		n++;
 	}
-	while (n >= 98)
+	while (n > 98)
 	{
 		printf("%d", n);
 		if (n != 98)
@@ -28,4 +28,5 @@ void print_to_98(int n)
 		}
 		n--;
 	}
+	printf("98");
 }
