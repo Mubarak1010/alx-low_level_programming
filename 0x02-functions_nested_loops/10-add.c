612c5766bf5045c31240606117ae70700e73074c
Mubarak1010 <anoze941@gmail.com>
2023-05-24 13:26:46 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Program entry
+ * @a: first number
+ * @b: second number
+ * Return: result of addition
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
