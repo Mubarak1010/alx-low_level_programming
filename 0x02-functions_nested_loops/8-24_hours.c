1221509f1974c1e8e2c89b255c417829f9e2dafc
Mubarak1010 <anoze941@gmail.com>
2023-05-28 13:42:00 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Program entry
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48;  b <= 51; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
