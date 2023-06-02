5db39f0a4c4821b2647adb05fd55ec55b488344e
Mubarak1010 <anoze941@gmail.com>
2023-06-02 13:57:51 -0700

    7-main.c

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * main - check the code for
- * Return: Always 0.
+ * leet - function that encodes a string into 1337
+ * @s: String in consideration
+ * Return: Pointer to the string.
  */
 
-int main(void)
+char *leet(char *s)
 {
-	char s[] = "Expect the bestPrepare for the worst.Capitalize on what comes.\n";
-	char *p;
+	int i = 0, j;
+	char a1[] = "aAeEoOtTlL";
+	char a2[] = "4433007711";
 
-	p = leet(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
-	_putchar('\n');
+	while (s[i] != '\0')
+	{
+		for (j = 0; j <= 10; j++)
+		{
+			if (s[i] == a1[j])
+				s[i] = a2[j];
+		}
+		i++;
+	}
+	return (s);
 }
