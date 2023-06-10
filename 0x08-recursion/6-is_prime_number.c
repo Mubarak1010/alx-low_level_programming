0b7eaf693e4894312c3d2d5a568297a7bc71a3ac
Mubarak1010 <anoze941@gmail.com>
2023-06-10 03:35:28 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,12 @@
 
 int is_prime_number(int n)
 {
-	if (n == 2 || n == 3)
+	if (n == 1 || n <= 0)
+		return (0);
+	else if (n == 2 || n == 3)
 		return (1);
 	else if (n % 2 != 0 && n % 3 != 0)
 		return (1);
-	else if (n == 1 || n <= 0)
-		return (0);
 	else
 		return (0);
 }
