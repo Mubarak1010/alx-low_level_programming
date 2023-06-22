0a278bf14c1013317969ca52ab48d910ef6b432d
Mubarak1010 <anoze941@gmail.com>
2023-06-22 13:46:50 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-print_numbers.c -o b

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%d", va_arg(num, int));
 			printf("%s", separator);
