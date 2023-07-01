77ebfa9d89ef85e799e1b74d89ed893d8565d76d
Mubarak1010 <anoze941@gmail.com>
2023-07-01 06:41:35 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,15 +9,7 @@
 
 int main(int argc, char **argv)
 {
-	int count;
-
-	count = 0;
-
-	if (argc > 0)
-	{
-		while (argv[count] != '\0')
-			count++;
-	}
-	printf("%d\n", count);
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
