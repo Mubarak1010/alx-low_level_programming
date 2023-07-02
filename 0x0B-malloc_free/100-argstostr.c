c943b527178755e05d5310a694716f171c2b2bd5
Mubarak1010 <anoze941@gmail.com>
2023-07-02 07:15:16 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - program entry
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int c = 0, i = 0, j = 0, k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (i < ac)
+	{
+		while (av[i][j])
+		{
+			c++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+
+	str = malloc((sizeof(char) * ch) + ac + 1);
+
+	i = 0;
+
+	while (av[i])
+	{
+		while (av[i][j])
+		{
+			str[k] = av[i][j];
+			k++;
+			j++;
+		}
+		str[k] = '\n';
+
+		j = 0;
+		k++;
+		i++;
+	}
+	k++;
+	str[k] = '\0';
+	return (str);
+}
