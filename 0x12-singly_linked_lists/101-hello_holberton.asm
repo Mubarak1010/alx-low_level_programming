e83c0b0f007036ff0e9f1a79163d97cd84a52ef8
Mubarak1010 <anoze941@gmail.com>
2023-07-03 14:26:33 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-global _start
-
-section .data
-string: db "Hello, Holberton", 10, 0
-length: equ $-string
-
-section .text
-_start:
-mov rax, 1
-mov rdi, 1
-mov rsi, string
-mov rdx, length
-syscall
-
-mov rax, 60
-mov rdi, 0
-syscall
+	global  main
+	extern  printf
+main:
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
+format: db 'Hello, Holberton', 10, 0
