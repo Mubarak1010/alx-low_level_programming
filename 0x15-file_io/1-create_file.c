e0114f2a151da23069590a7d289f95e4fdb3f03f
Mubarak1010 <anoze941@gmail.com>
2023-07-11 03:46:54 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_file - function that creates a file.
+ * @filename: File name
+ * @text_content: Text content
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *file;
+
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = fopen(filename, "w");
+
+	if (file != NULL)
+	{
+		while (text_content[i] != '\0')
+		{
+			putc(text_content[i], file);
+			i++;
+		}
+		fclose(file);
+	}
+	else
+	{
+		return (-1);
+	}
+	return (1);
+}
