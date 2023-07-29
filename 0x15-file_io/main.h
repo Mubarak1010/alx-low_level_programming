231dcf238fc039c9860b178f52b8bf4cb6aae53f
Mubarak1010 <anoze941@gmail.com>
2023-07-29 15:22:05 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,9 @@
 #define MAIN_h
 #include <stddef.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
