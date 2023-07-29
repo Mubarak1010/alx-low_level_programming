3ec02d2cfa2150b32b97019e929116f4dd424787
Mubarak1010 <anoze941@gmail.com>
2023-07-29 15:14:15 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,28 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - reads text file and prints it to the POSIX standard output.
- * @filename: File name
- * @letters: the number of letters it should read and print.
- * Return: the actual number of letters it could read and print.
- * 0 ---
- * If the file can not be opened or read,
- * If filename is NULL,
- * If write fails or does not write the expected amount of bytes
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t count = 0;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	FILE *file;
-
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	file = fopen(filename, "r");
-
-	if (file != NULL)
-	{
-		char c;
-
-		while ((c = fgetc(file)) != EOF && letters > 0)
-		{
-			printf("%c", c);
-			count++;
-			letters--;
-		}
-		fclose(file);
-	}
-	else
-	{
-		return (0);
-	}
-	return (count);
+	free(buf);
+	close(fd);
+	return (w);
 }
